a39e9460a15b4436275a670e7df311caf52db078
amanar3ra <amanarora3dec@gmail.com>
2025-03-11 15:08:03 -0300

    smalls

--- main.js
+++ main.js
@@ -16,12 +16,12 @@ const app = Fastify({
 const start = async () => {
   try {
     // Register the CORS plugin first
-    await app.register(fastifyCors, { 
+    await app.register(fastifyCors, {
       origin: ['https://67cf499aef623b2264aa7caf--mybooksdata.netlify.app', 'https://mybooksdata.netlify.app'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       credentials: true
     });
-    
+
     // Register MongoDB plugin
     await app.register(fastifyMongodb, {
       forceClose: true,
@@ -30,7 +30,7 @@ const start = async () => {
 
     // Define routes after plugins are registered
     // All your endpoint code here...
-    
+
     //Endpoint - To get all the data
     app.get('/', async (req, res) => {
       if (!app.mongo.client) {
@@ -42,87 +42,88 @@ const start = async () => {
       res.send(music);
     });
 
-//Endpoint - To get specific data using object Id
-app.get('/:musicId', async (req, res) => {
-  try{
-      const musicId = req.params.musicId;
-      const collection = app.mongo.db.collection('Music');
-      const music = await collection.findOne({_id: new ObjectID(musicId)});
+    //Endpoint - To get specific data using object Id
+    app.get('/:id', async (req, res) => {
+      try {
+        const musicId = req.params.id;
+        const collection = app.mongo.db.collection('Music');
+        const music = await collection.findOne({ _id: new ObjectID(`${id}`) });
 
-      if(music){
+        if (music) {
           res.send(music);
-      } else{
+        } else {
           res.status(404).send("Data not found");
+        }
       }
-  }
-  catch(error){
-      res.status(500).send("Internal Error");
-  }
-});
-
-//Endpoint - To add new song details
-app.post('/', postSongSchema, async (req, res) => {
-  try{
-      const collection = app.mongo.db.collection('Music');
-      const {error, value } = postSongSchema.validate(req.body);
-      if(error){
-          return res.status(400).send({error: error.details[0].message})
+      catch (error) {
+        res.status(500).send("Internal Error");
       }
+    });
 
-      const result = await collection.insertOne(value);
-      res.status(201).send({id: result.insertedId, message: 'Song added!'});
-  }catch(error) {
-      res.status(500).send({error: 'Unable to add song!'});
-  }},
-);
+    //Endpoint - To add new song details
+    app.post('/', postSongSchema, async (req, res) => {
+      try {
+        const collection = app.mongo.db.collection('Music');
+        const { error, value } = postSongSchema.validate(req.body);
+        if (error) {
+          return res.status(400).send({ error: error.details[0].message })
+        }
 
-//Endpoint - To update an existing song detail
-app.put('/:musicId', putSongSchema, async (req, res) => {
-  try{
-      const collection = app.mongo.db.collection('Music');
-      const musicId = req.params.musicId;
-      const {error, value} = putSongSchema.validate(req.body);
-      if(error){
-          return res.status(400).send({error: error.details[0].message});
+        const result = await collection.insertOne(value);
+        res.status(201).send({ id: result.insertedId, message: 'Song added!' });
+      } catch (error) {
+        res.status(500).send({ error: 'Unable to add song!' });
       }
+    },
+    );
 
-      const result = await collection.updateOne(
-          {_id: new ObjectID(musicId)},
-          {$set: value}
-      );
+    //Endpoint - To update an existing song detail
+    app.put('/:musicId', putSongSchema, async (req, res) => {
+      try {
+        const collection = app.mongo.db.collection('Music');
+        const musicId = req.params.musicId;
+        const { error, value } = putSongSchema.validate(req.body);
+        if (error) {
+          return res.status(400).send({ error: error.details[0].message });
+        }
 
-      if(result.matchedCount === 0){
-          res.status(400).send({error: 'Song not found'});
-      }else{
-          res.send({message: 'Song Updated'});
+        const result = await collection.updateOne(
+          { _id: new ObjectID(musicId) },
+          { $set: value }
+        );
+
+        if (result.matchedCount === 0) {
+          res.status(400).send({ error: 'Song not found' });
+        } else {
+          res.send({ message: 'Song Updated' });
+        }
+      } catch (error) {
+        res.status(500).send({ error: 'Unable to update' });
       }
-  }catch(error) {
-      res.status(500).send({error : 'Unable to update'});
-  }
-});
+    });
 
-//Endpoint - To delete one song detail
-app.delete('/:musicId', async (req, res) => {
-  try{
-      const collection = app.mongo.db.collection('Music');
-      const musicId = req.params.musicId;
+    //Endpoint - To delete one song detail
+    app.delete('/:musicId', async (req, res) => {
+      try {
+        const collection = app.mongo.db.collection('Music');
+        const musicId = req.params.musicId;
 
-      const result = await collection.deleteOne({_id: new ObjectID(musicId)});
-      if(result.deletedCount === 0){
-          res.status(404).send({error: 'Song not found'});
-      }else{
-          res.send({message: 'Song deleted'});
+        const result = await collection.deleteOne({ _id: new ObjectID(musicId) });
+        if (result.deletedCount === 0) {
+          res.status(404).send({ error: 'Song not found' });
+        } else {
+          res.send({ message: 'Song deleted' });
+        }
+      } catch (error) {
+        res.status(500).send({ error: 'Unable to delete song' });
       }
-  }catch(error){
-      res.status(500).send({error: 'Unable to delete song'});
-  }
-});
+    });
 
     // Start the server
     const PORT = process.env.PORT || 3000;
     await app.listen({ port: PORT, host: '0.0.0.0' });
     console.log(`Server running on port ${PORT}`);
-    
+
   } catch (err) {
     app.log.error(err);
     process.exit(1);
